c166aebaef18979b810815a597cb64cf9c31fa19
vrd1an <ferdian.akbar1609@gmail.com>
2024-10-30 20:25:55 +0700

    update patch5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./admin/**/*.{html,js}",
+        "./login/**/*.{html,js}",
+        "./register/**/*.{html,js}",
+        "./profile/**/*.{html,js}",
+        "./index.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
